57a542213b39994583559122f1ca5b9abac4ab35
turer73 <63844106+turer73@users.noreply.github.com>
2026-09-07 17:09:34 +0300

    Dördüncü editoryal SVG sahne grubunu ekle

---
+++ assets/visuals-scenes-4.js
@@ -0,0 +1,52 @@
+window.ITVisualScenes=Object.assign(window.ITVisualScenes||{}, {
+    'gokteki-ayi': `
+      <circle class="paper" cx="625" cy="116" r="52" opacity=".15"/>
+      <g class="accent"><circle cx="172" cy="294" r="9"/><circle cx="245" cy="235" r="12"/><circle cx="332" cy="265" r="8"/><circle cx="420" cy="185" r="11"/><circle cx="504" cy="223" r="8"/><circle cx="584" cy="154" r="12"/><circle cx="664" cy="208" r="9"/></g>
+      <path class="line" d="M172 294l73-59 87 30 88-80 84 38 80-69 80 54"/>
+      <path class="paper" d="M155 332c35-63 88-94 158-92 46 1 82 18 111 49 43 45 82 51 132 31 56-22 105-10 148 37-34 8-59 29-74 62H208c-10-35-28-64-53-87Z" opacity=".2"/>
+      <path class="inkline" d="M260 336c32-24 64-29 96-16M508 333c29-20 58-23 87-9" opacity=".55"/>
+      <circle class="paper" cx="109" cy="105" r="5"/><circle class="paper" cx="280" cy="91" r="4"/><circle class="paper" cx="359" cy="117" r="5"/><circle class="paper" cx="723" cy="91" r="4"/>
+    `,
+    'gec-kalan-haberci': `
+      <path class="line" d="M101 355h598M101 355c123-91 224-131 303-120 100 14 143 81 295 120" stroke-dasharray="10 12"/>
+      <circle class="accent" cx="126" cy="355" r="14"/><circle class="paper" cx="674" cy="355" r="14"/>
+      <path class="paper" d="M306 273c14-59 55-95 111-95 45 0 77 23 99 61l-47 21c-12-20-29-30-52-30-31 0-52 19-61 55l-50-12Z"/>
+      <path class="accent" d="M355 278h105l31 76h-82l-28-42-26 42h-65l65-76Z"/>
+      <path class="dark" d="M375 174c0-29 20-50 47-50s47 21 47 50-20 50-47 50-47-21-47-50Z" opacity=".75"/>
+      <path class="paper" d="M487 228l88-71 17 21-80 84-25-34Z"/>
+      <path class="line" d="M173 121h168M173 153h128M579 289h92M599 318h72" opacity=".5"/>
+    `,
+    'kendi-kaniti': `
+      <path class="line" d="M400 87c111 0 201 71 201 158 0 46-25 88-66 117M400 413c-111 0-201-71-201-158 0-46 25-88 66-117" stroke-width="20" opacity=".45"/>
+      <path class="accent" d="M518 332l74 31-62 51 12-49-24-33ZM282 168l-74-31 62-51-12 49 24 33Z"/>
+      <path class="paper" d="M294 140h212v220H294z"/>
+      <path class="dark" d="M329 181h142v14H329zm0 36h117v14H329zm0 36h142v14H329zm0 36h98v14h-98z" opacity=".6"/>
+      <path class="accent" d="M356 322h88v18h-88z"/>
+      <circle class="bgcut" cx="400" cy="250" r="47" opacity=".16"/>
+    `,
+    'calinan-ates': `
+      <circle class="accent" cx="400" cy="221" r="78" opacity=".28"/>
+      <path class="accent" d="M400 90c39 51 60 89 60 124 0 42-27 73-60 73s-60-31-60-73c0-35 21-73 60-124Z"/>
+      <path class="paper" d="M400 151c19 28 29 49 29 68 0 20-13 36-29 36s-29-16-29-36c0-19 10-40 29-68Z" opacity=".82"/>
+      <path class="dark" d="M104 405c31-76 81-116 151-120 35-2 68 8 98 30-50 11-83 41-101 90H104Zm592 0c-31-76-81-116-151-120-35-2-68 8-98 30 50 11 83 41 101 90h148Z" opacity=".63"/>
+      <path class="line" d="M92 407h616M188 327l66-63M612 327l-66-63"/>
+      <circle class="paper" cx="166" cy="292" r="17" opacity=".56"/><circle class="paper" cx="634" cy="292" r="17" opacity=".56"/>
+    `,
+    'herkesin-saati': `
+      <path class="paper" d="M0 350c94-63 176-73 247-31 72 42 145 45 218 9 85-42 170-35 335 53v119H0V350Z" opacity=".16"/>
+      <path class="line" d="M0 392c106-52 204-55 294-8s182 47 278 0 172-43 228-8M0 435h800" opacity=".5"/>
+      <g class="accent"><circle cx="238" cy="120" r="10"/><circle cx="285" cy="91" r="8"/><circle cx="334" cy="126" r="9"/><circle cx="267" cy="160" r="7"/><circle cx="318" cy="172" r="8"/><circle cx="373" cy="145" r="7"/></g>
+      <g class="line" opacity=".5"><path d="M238 120l47-29 49 35-67 34 51 12 55-27"/></g>
+      <circle class="paper" cx="590" cy="116" r="58" opacity=".12"/>
+      <path class="accent" d="M534 354c18-81 59-124 122-129-28 33-42 76-42 129h-80Z" opacity=".74"/>
+      <path class="inkline" d="M563 356c-4-54 7-93 34-118"/>
+    `,
+    'yilin-iki-kapisi': `
+      <circle class="accent" cx="400" cy="108" r="58" opacity=".72"/>
+      <path class="paper" d="M122 405V213c0-78 61-132 142-132s142 54 142 132v192h-72V226c0-43-29-75-70-75s-70 32-70 75v179h-72Z" opacity=".8"/>
+      <path class="paper" d="M394 405V213c0-78 61-132 142-132s142 54 142 132v192h-72V226c0-43-29-75-70-75s-70 32-70 75v179h-72Z" opacity=".8"/>
+      <path class="line" d="M74 405h652M264 81v324M536 81v324" opacity=".5"/>
+      <path class="accent" d="M221 335h86v70h-86zM493 335h86v70h-86z" opacity=".52"/>
+      <path class="inkline" d="M225 335c12-49 25-82 39-98 16 20 30 53 39 98M497 335c12-49 25-82 39-98 16 20 30 53 39 98"/>
+    `
+});
